1395b6d1738fce3cc9963ef0af1f5856d9656224
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-09 16:29:24 -0400

    Estrutura de dados em Lista com dois proffys

--- projetoproffy/src/server.js
+++ projetoproffy/src/server.js
@@ -1,5 +1,31 @@
 // Servidor Dependencies Express
 
+// Pegando os dados de maneira dinâmica do arquivo study.html
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "89987654534", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Leonardo Oliveira", 
+        avatar: "https://avatars2.githubusercontent.com/u/63181989?s=400&u=146ea59ffafacbd883d75c6c96b8a7b0b3dd5fbf&v=4", 
+        whatsapp: "65974863511", 
+        bio: "É hora de escolher a pílula vermelha", 
+        subject: "Filosofia", 
+        cost: "120", 
+        weekday: [3], 
+        time_from: [25200], 
+        time_to: [900]
+    }
+]
+
 // Variável constante 'express' com a função Express
 const express = require('express') 
 // Variável constante 'server' que executa a função Express
