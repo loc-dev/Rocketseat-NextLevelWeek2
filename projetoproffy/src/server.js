ed1934b53ffd94c48534a2fc49f2f9dbdfdc751b
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-09 15:51:16 -0400

    Configurando no Servidor que a pasta public fosse a pasta raiz

--- projetoproffy/src/server.js
+++ projetoproffy/src/server.js
@@ -1,11 +1,17 @@
 // Servidor Dependencies Express
-require('express')()
-.get("/", (req, res) => {  // Função curta
+
+// Variável constante 'express' com a função Express
+const express = require('express') 
+// Variável constante 'server' que executa a função Express
+const server = express()
+
+server.use(express.static("public"))        // .use : Configuração do nosso servidor
+
+.get("/", (req, res) => {                   // () = > {} : Função curta
     return res.sendFile(__dirname + "/views/index.html")
 })
 .get("/study", (req, res) => {
     return res.send("Study")
 })
+
 .listen(5500)
-
-
