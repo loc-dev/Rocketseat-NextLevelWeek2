df41c3e5e49f2674cbfd4c9ae63fba5b35eee87e
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-09 15:21:12 -0400

    Compreendendo com outro argumento de rota para o GET, instalando a dependencies Nodemon

--- projetoproffy/src/server.js
+++ projetoproffy/src/server.js
@@ -3,4 +3,7 @@ require('express')()
 .get("/", (req, res) => {  // Função curta
     return res.send("Hi from NLW")
 })
+.get("/", (req, res) => {
+    return res.send("Página study")
+})
 .listen(5500)
