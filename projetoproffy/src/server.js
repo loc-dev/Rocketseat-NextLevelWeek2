975804f1479a19de1fbc402a71c7a3ea793a1c85
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-08-10 20:01:05 -0400

    Refatorando e criando arquivos backups como fake_data.js, ajustando page-study.css com condicao de nenhum resultado encontrado aplicando estilizacao

--- projetoproffy/src/server.js
+++ projetoproffy/src/server.js
@@ -3,97 +3,12 @@
 
 // Servidor Dependencies Express
 
-// Dados
-// Pegando os dados de maneira dinâmica do arquivo study.html
-const proffys = [
-    { 
-        name: "Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "89987654534", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0], 
-        time_from: [720], 
-        time_to: [1220]
-    },
-    { 
-        name: "Leonardo Oliveira", 
-        avatar: "https://avatars2.githubusercontent.com/u/63181989?s=400&u=146ea59ffafacbd883d75c6c96b8a7b0b3dd5fbf&v=4", 
-        whatsapp: "65974863511", 
-        bio: "É hora de escolher a pílula vermelha", 
-        subject: "Filosofia", 
-        cost: "120", 
-        weekday: [3], 
-        time_from: [25200], 
-        time_to: [900]
-    }
-]
-
-const subjects = [    
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação Física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [    
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-// Funcionalidades para recarregamento das páginas em HTML
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    // Receber os dados e mandar de volta
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    // Se tiver dados :
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-        // Adicionar dados a Lista de Proffys
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-    
-    // Senão :
-    // Não adicionar e mostrar a página
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
 // Variável constante 'express' com a função Express
 const express = require('express') 
 // Variável constante 'server' que executa a função Express
 const server = express()
+
+const  { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
 
 // Configuração do Nunjucks (Template Engine)
 const nunjucks = require('nunjucks')
